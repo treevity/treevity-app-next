169ce80758c1365a263cdfd8001618d27feb7e86
Vladimír Fojtík <fojtik.v@gmail.com>
2019-08-12 08:10:44 +0200

    Added Ant Design into the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import { Col, Row } from 'antd';
 import { NextPage } from 'next';
 import * as React from 'react';
 import NextI18Instance from '~/i18n';
 
-const Home: NextPage = ({ t }: any) => <h1>{t('hello-world')}</h1>;
+const Home: NextPage = ({ t }: any) => (
+    <Row>
+        <Col span={12}>
+            <h1>{t('hello-world')}</h1>
+        </Col>
+        <Col span={12}>
+            <h1>{t('hello-world')}</h1>
+        </Col>
+    </Row>
+);
 
 export default NextI18Instance.withTranslation('common')(Home);
