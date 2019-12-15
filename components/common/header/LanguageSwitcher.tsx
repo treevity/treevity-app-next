67057e2d1c18c134fef09c789a7d064b4c9627e0
Fir3st <fojtik.v@gmail.com>
2019-12-15 13:35:21 +0100

    Finished basic language switcher

--- components/common/header/LanguageSwitcher.tsx
+++ components/common/header/LanguageSwitcher.tsx
@@ -1,10 +1,25 @@
 import { Col } from 'antd';
-import React from 'react';
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import I18n from '~/i18n';
 
-const LanguageSwitcher = () => {
+const Item = styled.span`
+    margin-right: 15px;
+    cursor: pointer;
+`;
+
+const LanguageSwitcher: FC = () => {
+    const { allLanguages } = I18n.config;
+    const { t, i18n } = useTranslation('languages');
+
+    const onChangeLocale = async (language: string) => {
+        await i18n.changeLanguage(language);
+    };
+
     return (
         <Col span={6}>
-            Česky / English
+            {allLanguages.map((language) => (<Item key={language} onClick={() => onChangeLocale(language)}>{t(language)}</Item>))}
         </Col>
     );
 };
--- components/common/header/User.tsx
+++ components/common/header/User.tsx
@@ -1,15 +1,18 @@
 import { Col } from 'antd';
-import React from 'react';
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 const StyledCol = styled(Col)`
     text-align: right;
 `;
 
-const User = () => {
+const User: FC = () => {
+    const { t } = useTranslation();
+
     return (
         <StyledCol span={6}>
-            <strong>User:&nbsp;</strong>
+            <strong>{t('user')}:&nbsp;</strong>
             <span>Vladimír Fojtík</span>
         </StyledCol>
     );
